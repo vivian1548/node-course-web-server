3488c93d7e1f3e79738c96d046e0586474c0421a
vivian <vivian1548@gmail.com>
2017-03-26 21:26:44 +0530

    port changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs=require('hbs');
-
+const port =process.env.PORT || 3000;
 var app =express();
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view-engine','hbs');
@@ -9,9 +9,9 @@ return  new Date().getFullYear()
 })
 
 
-app.use('/',(re,res,next)=>{
-  res.render('maintenance.hbs');
-});
+// app.use('/',(re,res,next)=>{
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname+'/public'));
 app.get('/',(req,res)=>{
@@ -34,4 +34,4 @@ app.get('/bad',(req,res)=>{
     errorCode:324
   });
 });
-app.listen(3000);
+app.listen(port);
